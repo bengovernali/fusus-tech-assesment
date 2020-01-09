01e0e2a2218fd0c03234857af8f87f3a2b9a2f7c
Ben Governali <bgovern93@gmail.com>
2020-01-09 11:55:54 -0500

    got product 'images' to display on the home page. the urls in the mock data dont work since no actual size is declared in the urls. will have to split the string and insert a custom size before displaying to the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
@@ -16,6 +17,7 @@ import { MatButtonModule } from "@angular/material/";
     BrowserModule,
     NoopAnimationsModule,
     MatButtonModule,
+    HttpClientModule,
     RouterModule.forRoot([{ path: "", component: ProductGridComponent }])
   ],
   providers: [],
--- src/app/product-grid/product-grid.component.ts
+++ src/app/product-grid/product-grid.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 
+import { ProductService } from "../product.service";
+
 @Component({
   selector: "app-product-grid",
   templateUrl: "./product-grid.component.html",
   styleUrls: ["./product-grid.component.css"]
 })
 export class ProductGridComponent implements OnInit {
-  constructor() {}
+  products;
 
-  ngOnInit() {}
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.products = this.productService.getProducts();
+  }
 }
